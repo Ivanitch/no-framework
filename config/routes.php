150e8f652d1a753f5cc1947c99c3ad717bc24635
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-03-04 23:11:42 +0900

    Router

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+return [
+    ['GET', '/', function () {
+        echo 'Home page';
+    }],
+    ['GET', '/hello-world', function () {
+        echo 'Hello World';
+    }],
+    ['GET', '/another-route', function () {
+        echo 'This works too';
+    }],
+];
