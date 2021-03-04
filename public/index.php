b26196e77a28aab550f3cbe20195c1178dbca51b
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-03-04 19:37:56 +0900

    Error Handler

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
+<?php declare(strict_types=1);
 
 require __DIR__ . '/../src/bootstrap.php';
+
+echo 'Hello World!';
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,19 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types=1);
+error_reporting(E_ALL);
 
-echo 'Hello World!';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$environment = 'development';
+
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new \Exception;
