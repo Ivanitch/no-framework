4367665b329d61da0a937609d93f89d459a209b3
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-05-23 00:23:22 +0900

    Changed the name of the request in the container

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use DI\Container;
 use Framework\ErrorHandler;
+use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Psr\Http\Message\ServerRequestInterface;
@@ -24,7 +25,7 @@ $router = require_once __DIR__ . '/router.php';
 try {
     // Request
     /** @var ServerRequestInterface $request $request */
-    $request = $container->get('Request');
+    $request = $container->get(Request::class);
     $response = $router->dispatch($request);
 } catch (Exception $e) {
     $response = new HtmlResponse('Undefined page', 404);
--- config/dependencies/http.php
+++ config/dependencies/http.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
+use Laminas\Diactoros\Request;
 use Laminas\Diactoros\ServerRequestFactory;
 use function DI\factory;
 
 return [
-    'Request' => factory(function () {
+    Request::class => factory(function () {
         return ServerRequestFactory::fromGlobals();
     })
 ];
